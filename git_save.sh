6461c14904eb02c1c8a8c3f01d1d8dfee2a22468
Tanveer Salim <tanveerasalim@gmail.com>
2018-12-12 01:51:12 -0600

    master

---
+++ git_save.sh
@@ -0,0 +1,19 @@
+# git_save.sh
+# Saves git local repository for Programming-Interview-Concepts
+# to GitHub
+
+# To run git_save.sh:
+# Type "bash git_save.sh"
+# in BASH and press ENTER.
+
+#!/bin/bash
+
+	#Download everything from GitHub to local repostory
+
+	git pull
+	
+	git add .
+
+	git commit -m master
+
+	git push origin master
